9e60e86e462ed0f7c76b0052705e3244e35ee1de
Shehzad Ahmed <41237363+Shehzad05@users.noreply.github.com>
2024-08-05 11:48:53 +0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,1 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/authLayout.module').then(m => m.AuthLayoutModule),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./layout/dashboard.module').then(m => m.DashboardModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
-];
